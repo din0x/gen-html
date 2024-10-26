1cad7d59b99cc3087f5ee56e8aeec31a1640f77a
din0x <superrobertxd52@gmail.com>
2024-10-26 16:18:42 +0200

    Use `create_element!`

--- src/element.rs
+++ src/element.rs
@@ -1,63 +1,14 @@
-//! HTML5 element builders.
-
 macro_rules! elem {
     ($doc:expr, $vis:vis $name:ident, $tag:ident, $content:tt, $($category:ty),*) => {
-        #[doc = concat!("Builder for the `<", stringify!($tag), ">` HTML element.\n",)]
-        #[doc = $doc]
-        #[derive(Default)]
-        $vis struct $name<T: $content> {
-            content: T,
-            class: $crate::attribute::ClassList,
-            id: Option<$crate::attribute::Id>,
-            lang: Option<$crate::attribute::Lang>,
-        }
-
-        impl<T: $content> $name<T> {
-            /// Sets the HTML `class` attribute.
-            pub fn class(mut self, class: $crate::attribute::ClassList) -> Self {
-                self.class = class;
-                self
-            }
-
-            /// Sets the HTML `id` attribute.
-            pub fn id(mut self, id: $crate::attribute::Id) -> Self {
-                self.id = Some(id);
-                self
-            }
-
-            /// Sets the HTML `lang` attribute.
-            pub fn lang(mut self, lang: $crate::attribute::Lang) -> Self {
-                self.lang = Some(lang);
-                self
-            }
-        }
-
-        impl<T: $content> $crate::Render for $name<T> {
-            fn render(self, target: &mut dyn std::fmt::Write) -> std::fmt::Result {
-                write!(target, concat!("<", stringify!($tag)))?;
-
-                if !self.class.is_empty() {
-                    write!(target, " class=\"{}\"", self.class)?;
-                }
-
-                self.id.as_ref().map(|id| write!(target, " id=\"{id}\""));
-                self.lang.as_ref().map(|lang| write!(target, " lang=\"{lang}\""));
-
-                write!(target, ">")?;
-                self.content.render(target)?;
-                write!(target, concat!("</", stringify!($tag), ">"))
-            }
+        $crate::generate::create_element!{
+            $doc,
+            $vis ($name, $tag): [$content],
+            []
         }
 
         $(
             impl<T: $content> $category for $name<T> {}
         )*
-
-        #[doc = concat!("The `<", stringify!($tag), ">` HTML element.\n",)]
-        #[doc = $doc]
-        $vis fn $tag<T: $content>(content: T) -> $name<T> {
-            $name { content, ..$name::<()>::default() }
-        }
     };
     ($vis:vis $name:ident, $tag:ident, $content:tt, $group:ident, $($category:ty),*) => {
         $crate::element::elem!(concat!(
