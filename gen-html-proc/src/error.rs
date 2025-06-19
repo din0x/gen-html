096fabfe158e0b59d3f732ab3ade44ed726437c4
din0x <superrobertxd52@gmail.com>
2025-06-19 14:27:34 +0200

    REFACTOR: Refactor the Error type

--- gen-html-proc/src/error.rs
+++ gen-html-proc/src/error.rs
@@ -1,45 +1,56 @@
 use proc_macro2::Span;
 use quote::{ToTokens, quote_spanned};
-use std::fmt;
+use syn::Ident;
 
-pub struct Error {
-    pub span: Span,
-    pub kind: ErrorKind,
+#[derive(Debug, Clone)]
+pub enum Error {
+    InvalidTag(Ident),
+    SelfClosing(Ident),
+    NotSelfClosing(Ident),
+    AttributeSpecifiedMoreThenOnce { spans: Vec<Span>, key: String },
+    Multi(Vec<Error>),
 }
 
 impl Error {
-    pub fn new(span: Span, kind: ErrorKind) -> Self {
-        Self { span, kind }
+    pub fn empty() -> Self {
+        Self::Multi(Vec::new())
+    }
+
+    pub fn is_empty(&self) -> bool {
+        matches!(self, Self::Multi(vec) if vec.is_empty())
+    }
+
+    pub fn push(&mut self, other: Self) {
+        if let Self::Multi(v) = self {
+            v.push(other);
+        } else {
+            *self = Self::Multi(vec![self.clone(), other])
+        }
     }
 }
 
 impl ToTokens for Error {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let msg = self.kind.to_string();
-
-        quote_spanned! {
-            self.span => compile_error!(#msg);
-        }
-        .to_tokens(tokens);
-    }
-}
-
-pub enum ErrorKind {
-    InvalidTag(String),
-    SelfClosing(String),
-    NotSelfClosing(String),
-    InvalidAttributeName(String),
-}
-
-impl fmt::Display for ErrorKind {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::InvalidTag(tag) => write!(f, "`{tag}` is not a valid HTML tag"),
-            Self::SelfClosing(tag) => write!(f, "`{tag}` must be used like a self closing tag"),
-            Self::NotSelfClosing(tag) => {
-                write!(f, "`{tag}` cannot be used like a self closing tag")
+            Self::InvalidTag(tag) => {
+                let msg = format!("`{tag}` is not a valid HTML tag");
+                quote_spanned!(tag.span() => compile_error!(#msg);).to_tokens(tokens);
             }
-            Self::InvalidAttributeName(name) => write!(f, "`{name}` is not a valid attribute"),
+            Self::SelfClosing(tag) => {
+                let msg = format!("`{tag}` must be used like a self closing tag");
+                quote_spanned!(tag.span() => compile_error!(#msg);).to_tokens(tokens);
+            }
+            Self::NotSelfClosing(tag) => {
+                let msg = format!("`{tag}` cannot be used like a self closing tag");
+                quote_spanned!(tag.span() => compile_error!(#msg);).to_tokens(tokens);
+            }
+            Self::AttributeSpecifiedMoreThenOnce { spans, key: name } => {
+                let msg = format!("attribute `{name}` specified more then once");
+                for span in spans {
+                    quote_spanned!(*span => compile_error!(#msg);).to_tokens(tokens);
+                }
+            }
+            Self::Multi(errors) => errors.iter().for_each(|e| e.to_tokens(tokens)),
         }
     }
 }
